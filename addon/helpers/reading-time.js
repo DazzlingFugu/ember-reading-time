db229f13fdba742b42d57d06642fa3cb27fabbd7
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-09-17 12:37:23 +0200

    feat: add humanize-duration to translate output

--- addon/helpers/reading-time.js
+++ addon/helpers/reading-time.js
@@ -1,6 +1,19 @@
 import { helper } from '@ember/component/helper';
 import readingTime from 'reading-time';
+import humanizeDuration from 'humanize-duration';
 
 export default helper((params /*, hash*/) => {
-  return readingTime(params[0]);
+  let stats = readingTime(params[0] ?? '');
+  let options = params[1];
+  return humanizeDuration(
+    stats.time < 60000 ? 60000 : stats.time,
+    Object.assign(
+      {
+        round: true,
+        units: ['m'],
+        language: 'en',
+      },
+      options
+    )
+  );
 });
