e0da4af1702cecf62c22ca0751884b4c91c3a3a6
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-11-02 17:11:00 +0000

    fix: fix Embroider test scenarios
    These changes aim to fix the following errors:
    https://github.com/peopledoc/ember-reading-time/actions/runs/1409500930
    
    _Extract of the error:_
    
    ```
      - broccoliBuilderErrorStack: ModuleNotFoundError: Module not found:
      Error: Can't resolve 'stream' in '/home/runner/work/ember-reading-time/ember-reading-time/node_modules/reading-time/lib/stream.js'
    Did you mean './stream'?
    Requests that should resolve in the current directory need to start with './'.
    Requests that start with a name are treated as module requests and resolve
    within module directories (node_modules).
    If changing the source code is not an option there is also a resolve
    options called 'preferRelative' which tries to resolve these kind of
    requests in the current directory too.
    
    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core
    modules by default.
    This is no longer the case. Verify if you need this module and configure
    a polyfill for it.
    
    If you want to include a polyfill, you need to:
    	- add a fallback 'resolve.fallback: { "stream": require.resolve("stream-browserify") }'
    	- install 'stream-browserify'
    ```
    
    During the Embroider tests the Node.js module `stream` couldn't be found
    because, in these tests, Webpack v5 is used, instead of the v4 used by
    `ember-auto-import@v1` (see `yarn why webpack`).
    
    This code was telling Webpack to include the polyfill for `stream`:
    https://github.com/peopledoc/ember-reading-time/blob/v1.0.0/index.js#L7-L11
    
    But this was only understood by Webpack v4:
    - https://github.com/embroider-build/embroider/issues/1004#issuecomment-957904392
    - https://v4.webpack.js.org/configuration/node/#other-node-core-libraries
    
    In Webpack v5, Node.js polyfills are *NOT* automatically included:
    - https://gist.github.com/ef4/d2cf5672a93cf241fd47c020b9b3066a
    - https://github.com/ef4/ember-auto-import/blob/v2.2.3/docs/upgrade-guide-2.0.md#webpack-5-upgrade
    - https://webpack.js.org/configuration/node/
    
      > As of webpack 5, You can configure only `global`, `__filename` or
      > `__dirname` under `node` option. If you're looking for how to
      > polyfill `fs` alike in Node.js under webpack 5, please check
      > `resolve.fallback` for help.

--- config/ember-try.js
+++ config/ember-try.js
@@ -78,8 +78,20 @@ module.exports = async function () {
           },
         },
       },
-      embroiderSafe(),
-      embroiderOptimized(),
+      embroiderSafe({
+        npm: {
+          dependencies: {
+            'stream-browserify': '3',
+          },
+        },
+      }),
+      embroiderOptimized({
+        npm: {
+          dependencies: {
+            'stream-browserify': '3',
+          },
+        },
+      }),
     ],
   };
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const { maybeEmbroider } = require('@embroider/test-setup');
+
+/**
+ * `EMBROIDER_TEST_SETUP_OPTIONS` is set by the Embroider scenarios for `ember-try`:
+ * https://github.com/embroider-build/embroider/blob/v0.47.1/packages/test-setup/src/index.ts#L48-L90
+ */
+const IS_EMBROIDER_ENABLED = Boolean(process.env.EMBROIDER_TEST_SETUP_OPTIONS);
 
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
@@ -14,12 +21,27 @@ module.exports = function (defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app, {
     skipBabel: [
       {
         package: 'qunit',
       },
     ],
+
+    /* eslint-disable prettier/prettier */
+    packagerOptions: {
+      webpackConfig: IS_EMBROIDER_ENABLED === false ? {} : {
+        node: {
+          global: true,
+        },
+        resolve: {
+          fallback: {
+            // `stream-browserify` is added for the Embroider tests, see `config/ember-try.js`.
+            stream: require.resolve('stream-browserify') // eslint-disable-line
+          },
+        },
+      },
+    },
+    /* eslint-disable prettier/prettier */
   });
 };
